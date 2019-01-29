24357fb87c9a39b140967c6adeac320813e0be3b
Ronald Tran <33103495+ronaldran97@users.noreply.github.com>
2019-01-29 10:51:43 -0800

    Add files via upload

---
+++ hwOne.c/cmake-build-debug/CMakeFiles/hwOne_c.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hwOne_c.dir/main.c.o"
+  "hwOne_c.pdb"
+  "hwOne_c"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hwOne_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
