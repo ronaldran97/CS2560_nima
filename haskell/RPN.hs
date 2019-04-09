44a18c209d89dffc712b3e11c363172ae9b7c530
Ronald Tran <33103495+ronaldran97@users.noreply.github.com>
2019-04-09 16:30:02 -0700

    Add files via upload

---
+++ haskell/RPN.hs
@@ -0,0 +1,15 @@
+--solveRPN :: (Num a) => String -> a
+--type of function
+
+import Data.List  
+  
+solveRPN :: String -> Float  
+solveRPN = head . foldl foldingFunction [] . words  
+    where   foldingFunction (x:y:ys) "*" = (x * y):ys  
+            foldingFunction (x:y:ys) "+" = (x + y):ys  
+            foldingFunction (x:y:ys) "-" = (y - x):ys  
+            foldingFunction (x:y:ys) "/" = (y / x):ys  
+            foldingFunction (x:y:ys) "^" = (y ** x):ys  
+            foldingFunction (x:xs) "ln" = log x:xs  
+            foldingFunction xs "sum" = [sum xs]  
+            foldingFunction xs numberString = read numberString:xs  
